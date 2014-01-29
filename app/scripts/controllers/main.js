0904eb3028d90eff29c32f71b8e19299b19f06fd
Jérémy Buget <jbuget@octo.com>
2014-01-29 23:00:51 +0100

    Initialize Yeoman structure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('prettyPrismicApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
